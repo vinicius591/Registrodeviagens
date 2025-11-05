0d26edf7979de306e333b8ff00d006ef5c5d8b8e
vinicius591 <francisco.sousa4283@aluno.ce.gov.br>
2025-11-05 12:01:06 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -3,7 +3,7 @@ import tabulate
 def registrar_viagem(listaViagens):
     motorista = input("Digite o nome do motorista: ")
     destino = input("O seu destino: ")
-    distancia = float(input("distancia percorrida em km: "))
+    distancia = float(input("Distancia percorrida em km: "))
     gasto = float(input("Valor gasto em combustivel em R$: "))
     consumo = gasto / distancia
     viagem = {
@@ -16,13 +16,13 @@ def registrar_viagem(listaViagens):
     listaViagens.append(viagem)
     print("viagem registrada\n")
     
-def exibir_viagens(liataViagens):
-    if not liataViagens:
+def exibir_viagens(listaViagens):
+    if not listaViagens:
         print("Nenhuma viagem registrada\n")    
         return
     
     tabela = []
-    for v in liataViagens:
+    for v in listaViagens:
         tabela.append([
             v["motorista"],
             v["destino"],
@@ -30,5 +30,43 @@ def exibir_viagens(liataViagens):
             v["gasto"],
             round(v["consumo"], 2)         
         ])
-    print("\nLista de viagens")
-    print("")
+    print("\nLista de viagens: ")
+    print(tabulate(tabela, headers = ["motorista", "destino", "km", "gasto R$", "R$/km"], tablefmt="fancy_grid"))
+    
+def buscar_motorista(listaViagens):
+    nome = input("Digite o nome do motorsta: ")
+    resultados = [v for v in listaViagens if v["motorista"] == nome.lower()]
+    if not resultados:
+        print("\nNenhuma viagem com {nome}\n")
+        return
+    
+    tabela = []
+    for v in resultados:
+        tabela.append([
+            v["motorista"],
+            v["destino"],
+            v["distancia"],
+            v["gasto"],
+            round(v["consumo"], 2)
+        ])
+    print(f"Viagens feitas por {nome}: ")
+    print(tabulate(tabela, headers=["Motorista", "Destino", "Km", "Gasto R$", "R$/Km"], tablefmt="fancy_grid"))
+
+def viagem_mais_cara(listaViagens):
+    if not listaViagens:
+        print("\nNenhuma viagem registrada: ")
+        return
+    
+    mais_cara = max(listaViagens, key=lambda v: v["gasto"])
+
+    print("\nViagem mais cara: ")
+    print(f"Motorista: {mais_cara['motorista']}")
+    print(f"Destino: {mais_cara['destino']}")
+    print(f"Gasto: R$ {mais_cara['gasto']: .2f}")
+    
+def mdeia_consumo (listaViagens):
+    if not listaViagens:
+        print("\nNenhuma viagem registrada")
+        return
+    
+    media=sum (v["consumo"]for v in listaViagens / )
